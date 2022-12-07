7f8b168bc1d4f894a9572e55debba43983a00066
paulcarkner <paul.carkner@gmail.com>
2022-12-06 20:22:48 -0500

    Initial conversion to React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import Style from "./App.module.css";
+import "./Game.js";
 
-function App() {
+//Styles set programatically so App can be used as sub-module in portfolio
+const bodyStyles = {
+  width: "100vw",
+  height: "100vh",
+  margin: 0,
+  padding: 0,
+  overflow: "hidden",
+  fontFamily: "sans-serif",
+  fontSize: "min(7.5vw, 7.5vh, 48px)",
+  fontWeight: "bold",
+  backgroundColor: "#444",
+};
+
+const rootStyles = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "100%",
+};
+
+export default function App() {
+  //Set BODY and #root styles programatically
+  for (let key in bodyStyles) document.body.style[key] = bodyStyles[key];
+  for (let key in rootStyles) document.getElementById("root").style[key] = rootStyles[key];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className={Style.App}>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
-
-export default App;
---
+++ src/Game.js
@@ -0,0 +1,151 @@
+import $ from "jquery";
+
+$(document).keydown(function (e) {
+    var moved = false;
+    switch (e.which) {
+      case 37: // left
+        $("td:not(.empty)").each(function () {
+          var emptytd = $(this).prevAll(".empty").last();
+          var newtd = $(this);
+          if (emptytd.length > 0) {
+            moved = moveTile($(this), emptytd);
+            newtd = emptytd;
+          }
+          if (
+            newtd.prev().text() == newtd.text() &&
+            !newtd.prev().is(".merged")
+          ) {
+            moved = mergeTiles(newtd.prev(), newtd);
+          }
+        });
+        if (moved) createTile();
+        break;
+
+      case 38: // up
+        $("td:not(.empty)").each(function () {
+          var colNum = $(this).parent().children().index(this);
+          var rows = $(this).parent().prevAll();
+          var emptytd = $(this)
+            .parent()
+            .prevAll()
+            .children(":nth-child(" + (colNum + 1).toString() + ")")
+            .not(":not(.empty)")
+            .last();
+          var newtd = $(this);
+          if (emptytd.length > 0) {
+            moved = moveTile($(this), emptytd);
+            newtd = emptytd;
+          }
+          var mergetd = newtd.parent().prev().children().eq(colNum);
+          if (mergetd.text() == newtd.text() && !mergetd.is(".merged")) {
+            moved = mergeTiles(mergetd, newtd);
+          }
+        });
+        if (moved) createTile();
+        break;
+
+      case 39: // right
+        $($("td:not(.empty)").get().reverse()).each(function () {
+          var emptytd = $(this).nextAll(".empty").last();
+          var newtd = $(this);
+          if (emptytd.length > 0) {
+            moved = moveTile($(this), emptytd);
+            newtd = emptytd;
+          }
+          if (
+            newtd.next().text() == newtd.text() &&
+            !newtd.next().is(".merged")
+          ) {
+            moved = mergeTiles(newtd.next(), newtd);
+          }
+        });
+        if (moved) createTile();
+        break;
+
+      case 40: // down
+        $($("td:not(.empty)").get().reverse()).each(function () {
+          var colNum = $(this).parent().children().index(this);
+          var rows = $(this).parent().nextAll();
+          var emptytd = $(this)
+            .parent()
+            .nextAll()
+            .children(":nth-child(" + (colNum + 1).toString() + ")")
+            .not(":not(.empty)")
+            .last();
+          var newtd = $(this);
+          if (emptytd.length > 0) {
+            moved = moveTile($(this), emptytd);
+            newtd = emptytd;
+          }
+          var mergetd = newtd.parent().next().children().eq(colNum);
+          if (mergetd.text() == newtd.text() && !mergetd.is(".merged")) {
+            moved = mergeTiles(mergetd, newtd);
+          }
+        });
+        if (moved) createTile();
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    //if ($("td.empty").length == 0) endGame();
+    $(".merged").removeClass("merged");
+    e.preventDefault();
+  });
+
+  $(document).ready(function () {
+    $("td").addClass("empty");
+    createTile();
+    createTile();
+  });
+
+  function createTile() {
+    var tds = $("td.empty");
+    // if (tds.length == 0) {
+    //   endGame();
+    // }
+    var newTile = tds.eq(getRand(tds.length));
+    newTile.removeClass("empty").css("color", "inherit");
+    if (getRand(10) == 9) {
+      newTile.text("4").css("background-color", "hsla(22, 100%, 90%, 1)");
+    } else {
+      newTile.text("2").css("background-color", "hsla(22, 100%, 95%, 1)");
+    }
+  }
+
+  function moveTile(oldTile, newTile) {
+    newTile
+      .text(oldTile.text())
+      .removeClass("empty")
+      .css("background-color", oldTile.css("background-color"))
+      .css("color", oldTile.css("color"));
+    oldTile.text("").addClass("empty").css("background-color", "");
+    return true;
+  }
+
+  function mergeTiles(mergedTile, oldTile) {
+    var value = parseInt(oldTile.text()) * 2;
+    mergedTile
+      .text(value)
+      .css(
+        "background-color",
+        "hsla(22, 100%, " +
+          (100 - 5 * Math.log2(value)).toString() +
+          "%, 1)"
+      )
+      .css(
+        "color",
+        (value >= 2048 ? "white" : "")
+      )
+      .addClass("merged");
+    oldTile.text("").addClass("empty").css("background-color", "").css("color", "");
+    return true;
+  }
+
+  function getRand(x) {
+    return Math.floor(Math.random() * x);
+  }
+
+  function endGame() {
+    window.location.reload();
+  }
